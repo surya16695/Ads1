70f875bb0c43828242a51978ade1e58afd9132fe
Surya <suryasaginala@msitprogram.net>
2018-09-26 20:10:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1 Assignments/mod_3/3-sum/Solution.java
+++ ADS1 Assignments/mod_3/3-sum/Solution.java
@@ -25,64 +25,33 @@
 //         // System.out.println("MSIT");
 //         // System.out.println(Arrays.toString(arr));
 
-//         int a = 0;
-//         for (int j = 0; j < arr.length-2; j++) {
-//             for (int k = j+1; k < arr.length-1; k++) {
-//                 if (j != k) {
-//                     a = -1*(arr[j] + arr[k]);
-//                     // System.out.println("sum = "+a+" ->j = "+arr[j]+"k ="+arr[k]);
-//                     // if(a > 0){
-//                     //     a = -a;
-//                     // }else{
-//                     //     a = -1*a;
-//                     // }
-//                     int z = Binarysearch(arr, a,k+1);
-//                     if (z != -1) {
-//                         count++;
-//                         // System.out.println("Suryaaaaaaaaa"+count);
-//                         break;
-//                     }
-//                 } 
-                
-//             }
-//                 // System.out.print(a+"*****");
-//                 // System.out.println((Binarysearch(arr, a)));
-            
-//         }
-//         return count;
-//     }
-//     public static int Binarysearch(int[] arraysent, int number,int s) {
-//         int tempvar = 0;
-//         int value = 0;
-//         int start = s;
-//         int end = arraysent.length - 1;
-//         // System.out.println("a = "+number);
-//         for (int j = s; j < arraysent.length; j++) {
-//             // System.out.println(start + " *" + end + " "+ number);
-//             if(start < arraysent.length && end < arraysent.length)
-//                 tempvar = (start + end)/2;
-//             // System.out.println(arraysent[tempvar]);
-//             if (arraysent[tempvar] == number) {
-//                 value = arraysent[tempvar];
-//                 // System.out.println("value ="+ value);
-//                 return value;   
-//                 // break;
-//             } 
-
-//             if (arraysent[tempvar] < number) {
-//                 start = tempvar+1;
-//             }
-//             if (arraysent[tempvar] > number) {
-//                 end = tempvar-1;
-//             } 
-            
-//         }
-//         return -1;
-//     }
-// }
+// int a = 0; for (int j = 0; j < arr.length-2; j++) { for (int k = j+1; k <
+// arr.length-1; k++) { if (j != k) { a = -1*(arr[j] + arr[k]); //
+// System.out.println("sum = "+a+" ->j = "+arr[j]+"k ="+arr[k]); // if(a > 0){
+// //     a = -a; // }else{ //     a = -1*a; // } int z = Binarysearch(arr,
+// a,k+1); if (z != -1) { count++; // System.out.println("Suryaaaaaaaaa"+count);
+// break; } } } // System.out.print(a+"*****"); //
+// System.out.println((Binarysearch(arr, a))); } return count; } public static
+// int Binarysearch(int[] arraysent, int number,int s) { int tempvar = 0; int
+// value = 0; int start = s; int end = arraysent.length - 1; //
+// System.out.println("a = "+number); for (int j = s; j < arraysent.length; j++)
+// { // System.out.println(start + " *" + end + " "+ number); if(start <
+// arraysent.length && end < arraysent.length) tempvar = (start + end)/2; //
+// System.out.println(arraysent[tempvar]); if (arraysent[tempvar] == number) {
+// value = arraysent[tempvar]; // System.out.println("value ="+ value); return
+// value; // break; } if (arraysent[tempvar] < number) { start = tempvar+1; } if
+// (arraysent[tempvar] > number) { end = tempvar-1; } } return -1; } }
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         threeSum t = new threeSum();
@@ -129,7 +98,6 @@ class threeSum {
                     r--;
                 }
             }
-            
         }
         System.out.println(count);
     }
