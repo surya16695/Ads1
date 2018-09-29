f8e3e17c3c144d318e43ba06a2e2975163d1fea8
Surya <suryasaginala@msitprogram.net>
2018-09-29 13:02:54 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS1 Assignments/mod_6/Assignment-1/Solution.java
+++ ADS1 Assignments/mod_6/Assignment-1/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 class AddLargeNumbers {
     // Stack s = new Stack();
 
@@ -24,36 +25,48 @@ class AddLargeNumbers {
     }
 
     public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-        int sum = 0;
         Stack surya = new Stack();
         String[] s = new String[list1.size()];
         String[] m = new String[list2.size()];
         Stack teja = new Stack();
         for (int i = 0; i < list1.size(); i++) {
+
             s[i] = (list1.delete());
         }
-        for (int i = s.length - 1; i > 0; i--) {
-             surya.push(s[i]);
+        for (int i = s.length - 1; i > -1; i--) {
+
+            surya.push(s[i]);
          } 
-        for (int i = 0; i < list1.size(); i++) {
+        for (int i = 0; i < list2.size(); i++) {
+
             m[i] = (list2.delete());
         }
-        for (int i = m.length - 1; i > 0; i--) {
-            teja.push(s[i]);
+        for (int i = m.length - 1; i > -1; i--) {
+
+            teja.push(m[i]);
+
         }
-        if (list1.size() < list2.size()) {
+        System.out.println(teja.pop());
+        if (surya.size() < teja.size()) {
             
-            for (int i = 0; i < list2.size(); i++) {
+            for (int i = 0; i < teja.size(); i++) {
                 LinkedList fun = new LinkedList();
-                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop());
-                String lol = sum + "";
-                fun.insert(lol);
+                int sum = 0;
+                System.out.println(teja.pop());
+                System.out.println("***************");
+                System.out.println(surya.pop());
+                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop()) + (sum % 10);
+                int k = sum / 10;
+                fun.insert(k + "");  
                 return fun;
             }
         }else {
             LinkedList fun = new LinkedList();
-            for (int i = 0; i < list1.size(); i++) {
-                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop());
+            for (int i = 0; i < surya.size(); i++) {
+                int sum = 0;
+                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop()) + (sum % 10);
+                int k = sum / 10;
+                fun.insert(k + "");  
                 return fun;
 
             }
