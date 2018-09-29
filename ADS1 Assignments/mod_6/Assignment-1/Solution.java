ef4585d8f180d83ce7e2941140d3ffef6f8f5cdb
Surya <suryasaginala@msitprogram.net>
2018-09-29 13:05:35 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS1 Assignments/mod_6/Assignment-1/Solution.java
+++ ADS1 Assignments/mod_6/Assignment-1/Solution.java
@@ -1,83 +1,5 @@
 import java.util.Scanner;
-import java.util.*;
-class AddLargeNumbers {
-    // Stack s = new Stack();
 
-    public static LinkedList numberToDigits(String number) {
-        LinkedList obj = new LinkedList();
-        String[] lol = number.split("");
-        for (int i = 0; i < lol.length; i++) {
-            obj.insert(lol[i]);
-        }
-        // System.out.println(obj.size());
-        return obj;
-    }
-
-    public static String digitsToNumber(LinkedList list) {
-        String su = "";
-        for (int j = list.size(); j > 0; --j) {
-
-            su += list.delete();
-
-        }
-        return su;
-
-    }
-
-    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-        Stack surya = new Stack();
-        String[] s = new String[list1.size()];
-        String[] m = new String[list2.size()];
-        Stack teja = new Stack();
-        for (int i = 0; i < list1.size(); i++) {
-
-            s[i] = (list1.delete());
-        }
-        for (int i = s.length - 1; i > -1; i--) {
-
-            surya.push(s[i]);
-         } 
-        for (int i = 0; i < list2.size(); i++) {
-
-            m[i] = (list2.delete());
-        }
-        for (int i = m.length - 1; i > -1; i--) {
-
-            teja.push(m[i]);
-
-        }
-        System.out.println(teja.pop());
-        if (surya.size() < teja.size()) {
-            
-            for (int i = 0; i < teja.size(); i++) {
-                LinkedList fun = new LinkedList();
-                int sum = 0;
-                System.out.println(teja.pop());
-                System.out.println("***************");
-                System.out.println(surya.pop());
-                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop()) + (sum % 10);
-                int k = sum / 10;
-                fun.insert(k + "");  
-                return fun;
-            }
-        }else {
-            LinkedList fun = new LinkedList();
-            for (int i = 0; i < surya.size(); i++) {
-                int sum = 0;
-                sum += Integer.parseInt(teja.pop()) + Integer.parseInt(surya.pop()) + (sum % 10);
-                int k = sum / 10;
-                fun.insert(k + "");  
-                return fun;
-
-            }
-
-        }
-        // String lol = sum + "";
-        // fun.insert(lol);
-        return null;
-
-    }
-}
 
 public class Solution {
     public static void main(String[] args) {
@@ -98,10 +20,10 @@ public class Solution {
             case "addLargeNumbers":
                 pDigits = AddLargeNumbers.numberToDigits(p);
                 qDigits = AddLargeNumbers.numberToDigits(q);
-                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                LinkedList result = AddLargeNumbers
+                .addLargeNumbers(pDigits, qDigits);
                 System.out.println(AddLargeNumbers.digitsToNumber(result));
                 break;
         }
     }
-    
 }
