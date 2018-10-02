fd5856ab63f41b439532acc8ca60ef66c12ebc2b
Surya <suryasaginala@msitprogram.net>
2018-10-02 14:00:31 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS1 Assignments/mod_7/7.1 Steque/Solution.java
+++ ADS1 Assignments/mod_7/7.1 Steque/Solution.java
@@ -1,38 +1,46 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = Integer.parseInt(scan.nextLine());
-		Steque st = new Steque();
-		while(scan.hasNextLine()) {
-			String[] input = scan.nextLine().split(" ");
-			switch (input[0]) {
-				case "push" :
-				st.push(input[1]);
-				System.out.println(st);
-				break;
-				case "pop" :
-				try {
-					st.pop() ;
-					if (st.isEmpty()) {
-						System.out.println("Steque is empty.");
-					} else {
-					System.out.println(st);
-					}	
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-				case "enqueue" :
-				st.enqueue(input[1]);
-				System.out.println(st);
-				break;
-				default:
-				System.out.println();
-				st = new Steque();
-				break;
-			}
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        Steque st = new Steque();
+        while(scan.hasNextLine()) {
+            String[] input = scan.nextLine().split(" ");
+            switch (input[0]) {
+                case "push" :
+                st.push(input[1]);
+                System.out.println(st);
+                break;
+                case "pop" :
+                try {
+                    st.pop() ;
+                    if (st.isEmpty()) {
+                        System.out.println("Steque is empty.");
+                    } else {
+                    System.out.println(st);
+                    }   
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+                case "enqueue" :
+                st.enqueue(input[1]);
+                System.out.println(st);
+                break;
+                default:
+                System.out.println();
+                st = new Steque();
+                break;
+            }
 
-		}
-	}
+        }
+    }
 }
