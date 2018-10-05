e3f9602518aa2b96aa5947f29d134a657a47c5e4
Surya <suryasaginala@msitprogram.net>
2018-10-05 15:23:28 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1 Assignments/mod_10/10.1 LinkedList using Recursion/Solution.java
+++ ADS1 Assignments/mod_10/10.1 LinkedList using Recursion/Solution.java
@@ -1,24 +1,51 @@
 import java.util.*;
+/**
+ * Class for node.
+ */
 class Node {
 	String data;
 	Node next;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      val   The value
+	 */
 	Node(String val){
 		data = val;
 	}
 }
+/**
+ * List of linkeds.
+ */
 class LinkedList{
 	Node start;
 	int size;
+	/**
+	 * Constructs the object.
+	 */
 	LinkedList(){
 		start = null;
 		size = 0;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      i     { parameter_description }
+	 */
 	void insert(Node i){
 		if(size == 0){
 			start = i;
 			return;
 		}
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      index      The index
+	 * @param      n          { parameter_description }
+	 *
+	 * @throws     Exception  { exception_description }
+	 */
 	void insertAt(int index,Node n) throws Exception{
 		if(index < 0 || index > size){
 			throw new Exception("");
@@ -34,6 +61,16 @@ class LinkedList{
 		start =insertAt(index, start,n,0);
 
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      index    The index
+	 * @param      first    The first
+	 * @param      element  The element
+	 * @param      count    The count
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	Node insertAt(int index,Node first, Node element,int count ){
 		if(count == index){
 			element.next = first;
@@ -43,21 +80,35 @@ class LinkedList{
 		size++;
 		return first;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @throws     Exception  { exception_description }
+	 */
 	void reverse() throws Exception{
 		if(start == null){
 			throw new Exception();
 		}
 		reverse(null,start);
 	}
-void reverse( Node prev, Node current)
-{
-   if(current!=null){
-      reverse(current,current.next);
-      current.next = prev;
-    }else{
-    	start = prev;
-    }
-}
+	/**
+	 * { function_description }
+	 *
+	 * @param      prev     The previous
+	 * @param      current  The current
+	 */
+	void reverse( Node prev, Node current)
+	{
+	   if(current!=null){
+	      reverse(current,current.next);
+	      current.next = prev;
+	    }else{
+	    	start = prev;
+	    }
+	}
+	/**
+	 * { function_description }
+	 */
 	void display(){
 		Node temp = start;
 		String str="";
@@ -68,8 +119,16 @@ void reverse( Node prev, Node current)
 		System.out.println(str.substring(0,str.length()-2));
 	}
 }
+/**
+ * Class for solution.
+ */
 class Solution{
-	public static void main(String[] args) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		LinkedList ll = new LinkedList();
 		while(sc.hasNextLine()){
