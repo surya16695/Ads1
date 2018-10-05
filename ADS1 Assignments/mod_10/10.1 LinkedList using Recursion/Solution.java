822534046440da2b2d5fc5db4aa72840928f138b
Surya <suryasaginala@msitprogram.net>
2018-10-05 16:56:23 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1 Assignments/mod_10/10.1 LinkedList using Recursion/Solution.java
+++ ADS1 Assignments/mod_10/10.1 LinkedList using Recursion/Solution.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
  */
 class Solution {
     /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
      * { function_description }.
      *
      * @param      args  The arguments
