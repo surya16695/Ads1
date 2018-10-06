e35df51a063ad8f7acd719828923615d5d94fda1
Surya <suryasaginala@msitprogram.net>
2018-10-06 13:23:46 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- ADS1 Assignments/mod_12/Assignment-1/Solution.java
+++ ADS1 Assignments/mod_12/Assignment-1/Solution.java
@@ -4,38 +4,26 @@ import java.util.Arrays;
  * Class for solution.
  */
 class Solution {
-	/**
-	 * { var_description }
-	 */
-	private int SIX = 6;
-	/**
-	 * { var_description }
-	 */
-	private int FIVE = 5;
-	/**
-	 * { var_description }
-	 */
-	private int FOUR = 4;
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Merge m = new Merge();
-		int asize = (scan.nextInt());
-		Student[] stu = new Student[asize];
-		int vacancies = (scan.nextInt());
-		int unreserved = (scan.nextInt());
-		int bc = (scan.nextInt());
-		int st = (scan.nextInt());
-		int sc = (scan.nextInt());
-		for(int i = 0; i < asize; i++) {
-			String[] t = scan.nextLine().split(",");
-			stu[i] = new Student(t[0], t[1], Integer.parseInt(t[2]), Integer.parseInt(t[2 + 1]), Integer.parseInt(t[2 + 2]), Integer.parseInt(t[2 + 2 + 1]), t[2 + 2 + 2]);
-		}
-		System.out.println(Arrays.toString(stu));
-		//m.sort(stu);
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Merge m = new Merge();
+        int asize = (scan.nextInt());
+        Student[] stu = new Student[asize];
+        int vacancies = (scan.nextInt());
+        int unreserved = (scan.nextInt());
+        int bc = (scan.nextInt());
+        int st = (scan.nextInt());
+        int sc = (scan.nextInt());
+        for(int i = 0; i < asize; i++) {
+            String[] t = scan.nextLine().split(",");
+            stu[i] = new Student(t[0], t[1], Integer.parseInt(t[2]), Integer.parseInt(t[2 + 1]), Integer.parseInt(t[2 + 2]), Integer.parseInt(t[2 + 2 + 1]), t[2 + 2 + 2]);
+        }
+        System.out.println(Arrays.toString(stu));
+        //m.sort(stu);
+    }
 }
