2630c645ac9142b5130d8922eb7ee91b67d156a2
Surya <suryasaginala@msitprogram.net>
2018-10-06 07:02:11 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS1 Assignments/mod_9/9.1 Merge Sort Improvements/Solution.java
+++ ADS1 Assignments/mod_9/9.1 Merge Sort Improvements/Solution.java
@@ -1,138 +1,25 @@
-import java.util.*;
-/**
- * Class for solution.
- */
-class Solution{
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Merge m = new Merge();
-		while(sc.hasNextLine()) {
-			String[] a = sc.nextLine().split(",");
-			int[] number = new int[a.length];
-			for (int i = 0; i < a.length; i++) {
-				number[i] = Integer.parseInt(a[i]);
-			}
-			System.out.println(m.mergeSort(number));
-			System.out.println();
-		}
-	}
-}
-/**
- * Class for merge.
- */
-class Merge {
-	/**
-	 * { var_description }
-	 */
-	int size;
-	/**
-	 * { var_description }
-	 */
-	Insertion ins;
-	/**
-	 * Constructs the object.
-	 */
-	Merge() {
-		size = 0;
-		ins = new Insertion();
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      num   The number
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	int[] mergeSort(int[] num) {
-		int low = 0;
-		int max = num.length - 1;
-		int mid = (low + max)/2;
-		int[] temp1 = new int[num.length/2];
-		int[] temp2 = new int[num.length/2];
-
-		for (int i = 0; i < mid + 1; i++) {
-			temp1[i] = num[i];
-		}
-		for (int i = mid + 1; i < max; i++) {
-			temp2[i] = num[i];
-		}
-		if (temp1.length <= 7) {
-			System.out.println("Insertion sort method invoked...");
-			ins.Sort(temp1, temp1.length);
-		}
-		if (temp2.length <= 7) {
-			System.out.println("Insertion sort method invoked...");
-			ins.Sort(temp2, temp2.length);
-		} else if (temp1.length > 7 || temp2.length > 7) {
-			mergeSort(temp1);
-			mergeSort(temp2);
-		} if (temp1[temp1.length - 1] < temp2[temp2.length - 1]) {
-			
-		}
-		int[] sorted = merge(temp1, temp2, temp1.length, temp2.length);
-		return sorted;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      L     { parameter_description }
-	 * @param      R     { parameter_description }
-	 * @param      n1    The n 1
-	 * @param      n2    The n 2
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	int[] merge(int[] L, int[] R, int n1, int n2) {
-		int[] arr = new int[n1 + n2];
-        /* Merge the temp arrays */
-  
-        // Initial indexes of first and second subarrays 
-        int i = 0, j = 0; 
-  
-        // Initial index of merged subarry array 
-        int k = 1; 
-        while (i < n1 && j < n2) 
-        { 
-            if (L[i] <= R[j]) 
-            { 
-                arr[k] = L[i]; 
-                i++; 
-            } 
-            else
-            { 
-                arr[k] = R[j]; 
-                j++; 
-            } 
-            k++; 
-        } 
-  
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-            k++; 
-        } 
-  
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) 
-        { 
-            arr[k] = R[j]; 
-            j++; 
-            k++; 
-        } 
-        return arr; 
-	}
-
+import java.util.Scanner;
+/**MERGE.**/
+final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @param args value
+     * Time complexity is O(N).
+     */
+    public static void main(final String[] args) {
+        Merge m = new Merge();
+        Scanner s = new Scanner(System.in);
+        final int len = 50;
+        String[] tokens = new String[len];
+        while (s.hasNextLine()) {
+             tokens = s.nextLine().split(",");
+             m.sort(tokens);
+            System.out.println(m.show(tokens));
+            System.out.println();
+        }
+    }
 }
---
+++ ADS1 Assignments/mod_9/9.1 Merge Sort Improvements/merge.java
@@ -0,0 +1,113 @@
+import java.util.*;
+class Merge {
+    final int CUTOFF = 7;
+    Merge() {
+
+    }
+    /**
+     * Time complexity is O(N).
+     */
+    public void merge(Comparable[] array, Comparable[] newarray, int lo, int mid, int hi) {
+        // assert isSorted(array, lo, mid);
+        // assert isSorted(array, mid+1, hi);
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                newarray[k] = array[j++];
+            }
+
+            else if (j > hi) {
+                newarray[k] = array[i++];
+            }
+
+            else if (less(array[j], array[i])) {
+                newarray[k] = array[j++];
+            }
+            else {
+                newarray[k] = array[i++];
+            }
+        }
+        // assert isSorted(newarray, lo, hi);
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    private void sort(Comparable[] array, Comparable[] newarray, int lo, int hi) {
+        if (hi <= lo + CUTOFF) {
+            insertionSort(newarray, lo, hi);
+            System.out.println("Insertion sort method invoked...");
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(newarray, array, lo, mid);
+        sort(newarray, array, mid+1, hi);
+
+        // if (!less(array[mid+1], array[mid])) {
+        //    for (int i = lo; i <= hi; i++) {
+        //     newarray[i] = array[i];
+        //     }
+        //     System.out.println("Array is already sorted. So, skipped the call to merge...");
+        //    return;
+        // }
+        merge(array, newarray, lo, mid, hi);
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    public void sort(Comparable[] a) {
+        Comparable[] aux = a.clone();
+        sort(aux, a, 0, a.length-1);
+        assert isSorted(a);
+    }
+    /**
+     * Time complexity is O(N^2)
+     */
+    public void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j-1]); j--)
+                exch(a, j, j-1);
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    public void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    /**
+     * Time complexity is O(1)
+     */
+    public boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+    /**
+     * Time complexity is O(1).
+     */
+    public boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    /**
+     * Time complexity is O(N)
+     */
+    public boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        return true;
+    }
+    /**
+     * Time complexity is O(N)
+     */
+    public Object show(Object[] a) {
+        String s = "[";
+        int i;
+        for (i = 0; i < a.length - 1; i++) {
+            s += a[i] + ", ";
+        }
+        s = s + a[a.length - 1] + "]";
+        return s;
+    }
+}
