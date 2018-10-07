4a396f8384553bdd9f6b50db027b88339c7349d0
Surya <suryasaginala@msitprogram.net>
2018-10-07 17:00:02 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS1 Assignments/mod__12/Assignment-1/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Insertion in = new Insertion();
+        int asize = (scan.nextInt());
+        student[] stu = new student[asize];
+        int vacancies = (scan.nextInt());
+        int unreserved = (scan.nextInt());
+        int bc = (scan.nextInt());
+        int st = (scan.nextInt());
+        int sc = (scan.nextInt());
+        int size = 0;
+        scan.nextLine();
+        for (int i = 0; i < asize; i++) {
+            String[] t = scan.nextLine().split(",");
+            stu[i] = new student(t[0], t[1], Integer.parseInt(t[2]),
+             Integer.parseInt(t[2 + 1]),
+             Integer.parseInt(t[2 + 2]), Integer.parseInt(t[2 + 2 + 1]),
+              t[2 + 2 + 2]);
+            size++;
+        }
+        System.out.println(in.leaderboard(stu, size));
+    }
+}
---
+++ ADS1 Assignments/mod__12/Assignment-1/student.java
@@ -0,0 +1,125 @@
+/**STUDENT.**/
+class student implements Comparable<student> {
+    /**
+     * variable.
+     */
+    private String name;
+    /**
+     * variable.
+     */
+    private String dob;
+    /**
+     * variable.
+     */
+    private int sub1;
+    /**
+     * variable.
+     */
+    private int sub2;
+    /**
+     * variable.
+     */
+    private int sub3;
+    /**
+     * variable.
+     */
+    private int total;
+    /**
+     * variable.
+     */
+    private String reservation;
+    /**
+     * Constructs the object.
+     *
+     * @param      name   The name
+     * @param      dob    The dob
+     * @param      sub1   The sub 1
+     * @param      sub2   The sub 2
+     * @param      sub3   The sub 3
+     * @param      total  The total
+     * @param      res    The resource
+     */
+    student(String name,String dob,int sub1,int sub2,int sub3,int total,String res) {
+        this.name = name;
+        this.dob = dob;
+        this.sub1 = sub1;
+        this.sub2 = sub2;
+        this.sub3 = sub3;
+        this.total = total;
+        this.reservation = res;
+    }
+    /**
+     * time complexity is 1
+     */
+    public String getname() {
+        return this.name;
+    }
+    /**
+     * time complexity is 1
+     */
+    public String getdob() {
+        return this.dob;
+    }
+    /**
+     * time complexity is 1
+     */
+    public int get1() {
+        return this.sub1;
+    }
+    /**
+     * time complexity is 1
+     */
+    public int get2() {
+        return this.sub2;
+    }
+    /**
+     * time complexity is 1
+     */
+    public int get3() {
+        return this.sub3;
+    }
+    /**
+     * time complexity is 1
+     */
+
+    public int gettotal() {
+        return this.total;
+    }
+    /**
+     * time complexity is 1
+     */
+    public String getreserve() {
+        return this.reservation;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(student that) {
+        if (this.gettotal() > that.gettotal()) {
+            return 1;
+        }
+         else if (this.gettotal() < that.gettotal()) {
+            return -1;
+         }
+         else if (this.get3() > that.get3()) {
+            return 1;
+         } else if (this.get3() < that.get3()) {
+            return -1;
+         } else if (this.get2() > that.get2()) {
+            return 1;
+         } else if (this.get2() < that.get2()) {
+            return - 1;
+         } else if (this.getdob().compareTo(that.getdob()) > 0) {
+            return 1;
+         } else if (this.getdob().compareTo(that.getdob()) < 0) {
+            return -1;
+         }
+            else {
+        return 0;
+        }
+    }
+}
