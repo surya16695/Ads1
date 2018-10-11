29f02b9664a7b75765f1fca820d1b990db2fe152
Surya <suryasaginala@msitprogram.net>
2018-10-11 12:07:40 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1 Assignments/mod_16/16.1 BST/Solution.java
+++ ADS1 Assignments/mod_16/16.1 BST/Solution.java
@@ -109,12 +109,12 @@ class Solution {
 				case "get":
 				Book old1 = new Book(line[1], line[2], Float.parseFloat(line[3]));
 				Integer c = bt.get(old1);
-				if(c == null){
-					System.out.println("null");
-				}
-				else{
+				// if(c == null){
+				// 	System.out.println("null");
+				// }
 					System.out.println(c);
-				}
+				// else{
+				// }
 				
 				break;
 			}
