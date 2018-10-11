32713f8bbf8ce6c5b7403b83eed82424b4c5d5fe
Surya <suryasaginala@msitprogram.net>
2018-10-11 17:30:07 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1 Assignments/mod_16/16.1 BST/Solution.java
+++ ADS1 Assignments/mod_16/16.1 BST/Solution.java
@@ -1,32 +1,43 @@
 import java.util.Scanner;
 
-class Solution {
-	Solution() {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BinarySt bt = new BinarySt();
-		while (sc.hasNext()) {
-			String[] line = sc.nextLine().split(",");
-			switch (line[0]) {
-				// Node ne = new Node(old);
-				case "put":
-				Book old = new Book(line[1], line[2], Float.parseFloat(line[3]));
-				bt.put(old, Integer.parseInt(line[4]));
-				break;
-				case "get":
-				Book old1 = new Book(line[1], line[2], Float.parseFloat(line[3]));
-				Integer c = bt.get(old1);
-				// if(c == null){
-				// 	System.out.println("null");
-				// }
-					System.out.println(c);
-				// else{
-				// }
-				
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySt bt = new BinarySt();
+        while (sc.hasNext()) {
+            String[] line = sc.nextLine().split(",");
+            switch (line[0]) {
+                // Node ne = new Node(old);
+                case "put":
+                Book old = new Book(line[1], line[2], Float.parseFloat(line[1 + 2]));
+                bt.put(old, Integer.parseInt(line[2 + 2]));
+                break;
+                case "get":
+                Book old1 = new Book(line[1], line[2], Float.parseFloat(line[1 + 2]));
+                Integer c = bt.get(old1);
+                // if(c == null){
+                //  System.out.println("null");
+                // }
+                    System.out.println(c);
+                // else{
+                // }
+                
+                break;
+            }
+        }
+    }
 }
