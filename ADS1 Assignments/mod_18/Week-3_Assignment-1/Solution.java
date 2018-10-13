2604c2fc71c1df025caf43cfed5c45f03c47c54f
Surya <suryasaginala@msitprogram.net>
2018-10-13 13:21:48 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- ADS1 Assignments/mod_18/Week-3_Assignment-1/Solution.java
+++ ADS1 Assignments/mod_18/Week-3_Assignment-1/Solution.java
@@ -1,72 +1,82 @@
 import java.util.*;
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		sc.nextLine();
-		BinarysearchST<String, Integer> maxST = new BinarysearchST<String, Integer>();
-		BinarysearchST<String, Integer> minST = new BinarysearchST<String, Integer>();
-		while(!sc.hasNextInt()) {
-			int i = 0;
-			MaxPQ<Stock> max = new MaxPQ<Stock>();
-			MinPQ<Stock> min = new MinPQ<Stock>();
-			int range = N;
-			for (i = 1; i < range + 1; i++) {
-				String[] line = sc.nextLine().split(",");
-				if (line[0].equals("0")) {
-					break;
-				} else {
-				Stock stock = new Stock(line[0], Float.parseFloat(line[1]));
-				max.insert(stock);
-				min.insert(stock);
-				}
-			}
-			for (int j = 1; j <= 5; j++) {
-				int c = 1;
-				System.out.println(max.delMax());
-				// if (maxST.contains(max.delMax().getName())) {
-				// 	maxST.put(max.delMax().getName(), c++);
-				// }
-				// maxST.put(max.delMax().getName(), c);
-				// if (max.delMax() == null) {
-				// 	System.out.println("Surya******************");
-				// }
-			}
-			max = null;
-			System.out.println();
-			for (int j = 0; j <= 4; j++) {
-				System.out.println(min.delMin());
-			}
-			min = null;
-			System.out.println();
-			if (range < 7 * N) {
-				i = range;
-				range = range + range;
-				
-			}	
-		}
-		int q = sc.nextInt();
-		while (sc.hasNext()) {
-			String[] query = sc.nextLine().split(",");
-			switch (query[0]) {
-				case "get":
-					switch (query[1]) {
-					case "maxST":
-					maxST.get(query[2]);
-					break;
-					case "minST":
-					minST.get(query[2]);
-					break;
-				}
-				break;
-				case "intersection":
-				break;
-				default:
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * main .
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        BinarysearchST<String, Integer> maxST = new BinarysearchST<String, Integer>();
+        BinarysearchST<String, Integer> minST = new BinarysearchST<String, Integer>();
+        while(!sc.hasNextInt()) {
+            int i = 0;
+            MaxPQ<Stock> max = new MaxPQ<Stock>();
+            MinPQ<Stock> min = new MinPQ<Stock>();
+            int range = N;
+            for (i = 1; i < range + 1; i++) {
+                String[] line = sc.nextLine().split(",");
+                if (line[0].equals("0")) {
+                    break;
+                } else {
+                Stock stock = new Stock(line[0], Float.parseFloat(line[1]));
+                max.insert(stock);
+                min.insert(stock);
+                }
+            }
+            for (int j = 1; j <= 5; j++) {
+                int c = 1;
+                System.out.println(max.delMax());
+                // if (maxST.contains(max.delMax().getName())) {
+                //  maxST.put(max.delMax().getName(), c++);
+                // }
+                // maxST.put(max.delMax().getName(), c);
+                // if (max.delMax() == null) {
+                //  System.out.println("Surya******************");
+                // }
+            }
+            max = null;
+            System.out.println();
+            for (int j = 0; j <= 4; j++) {
+                System.out.println(min.delMin());
+            }
+            min = null;
+            System.out.println();
+            if (range < 7 * N) {
+                i = range;
+                range = range + range;
+            }
+        }
+        int q = sc.nextInt();
+        while (sc.hasNext()) {
+            String[] query = sc.nextLine().split(",");
+            switch (query[0]) {
+                case "get":
+                    switch (query[1]) {
+                    case "maxST":
+                    maxST.get(query[2]);
+                    break;
+                    case "minST":
+                    minST.get(query[2]);
+                    break;
+                }
+                break;
+                case "intersection":
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
