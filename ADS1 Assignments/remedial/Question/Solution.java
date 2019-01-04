650f95c58a1cb3dbb117913e0c2798272fa220ed
Surya <suryasaginala@msitprogram.net>
2019-01-04 14:21:37 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 Assignments/remedial/Question/Solution.java
+++ ADS1 Assignments/remedial/Question/Solution.java
@@ -13,8 +13,21 @@ class Solution {
 		while(sc.hasNextLine()) {
             String[] input = sc.nextLine().split(",");
         	if (input[0].equals("JC")) {
+        		Person[] pers = new Person[30];
 				s.sort();
+				// int k = 0;
 				Person[] per = s.jcArray();
+				for (int i = 0; i < s.size(); i++) {
+					if (pers[i] == null) {
+						pers[i] = per[i];
+					} else {
+						for (Person each: pers) {
+							if (each.getJid()  != per[i].getJid()) {
+								pers[i] = per[i];
+							}
+						}
+					}
+				}
         		System.out.println(per[count]);
         		count++;
         	}else {
