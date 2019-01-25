e7d45f1317af3180850564dd420948ec70f133f3
Surya <suryasaginala@msitprogram.net>
2019-01-25 11:46:22 +0530

    "ADS-1 remedial-2: 0/10 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 Assignments/remedial/Exam question/Solution.java
+++ ADS1 Assignments/remedial/Exam question/Solution.java
@@ -27,21 +27,36 @@ class Solution {
 			}
 			tree[i] = st;
 		}
-		// System.out.println(Arrays.toString(tree));
+		ST<Integer, String[]> finalst = new ST<Integer, String[]>();
+		List list = new List();
+		int x= 0;
 		for (int a = 0; a < 9 ; a++) {
 			int count = 0;
 			if (tree[a].contains(word)) {
 				String[] as = tree[a].get(word).split(" ");
-				toString(a,as);
+				finalst.put(a, as);
+				list.add(a);
+				x++;
+				// toString(a,as);
 				count++;	
+			}
+
+		}
+		System.out.println(word+":");
+		list.soRt();
+		String answer = "";
+		for (int b = 0; b < list.size(); b++) {
+			if (finalst.contains(list.get(b))) {
+				answer += toString(list.get(b), finalst.get(list.get(b)))+"\n";
 			}
 		}
 	}
 
-	public static void toString(int b, String[] arr) {
+	public static String toString(int b, String[] arr) {
 		String s = "";
 		s += b+", "+arr.length+", "+Arrays.toString(arr);
-		System.out.println(s);
+		// System.out.println(s);
+		return s;
 	}
 
 }
