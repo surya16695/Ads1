dfb97e4d8f04b9ac5203a9f8f2e2a4de354029c1
Surya <suryasaginala@msitprogram.net>
2019-01-25 11:18:30 +0530

    "ADS-1 remedial-2: 0/10 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 Assignments/remedial/Exam question/Solution.java
+++ ADS1 Assignments/remedial/Exam question/Solution.java
@@ -6,23 +6,24 @@ class Solution {
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		String word = sc.nextLine();
-		ST<String, Integer[]>[] tree = new ST[9];
+		ST<String, String>[] tree = new ST[9];
 		for (int i = 0; i < 9; i++) {
 			Scanner s = new Scanner(new File("files/"+ i + ".txt"));
-			ST<String, Integer[]> st = new ST<String, Integer[]>();
+			ST<String, String> st = new ST<String, String>();
 			String[] str = s.nextLine().split(" ");
 			// System.out.println(Arrays.toString(str));
 			for (int j = 0; j < str.length; j++) {
-				Integer[] ind = new Integer[str.length];
-				int x = 0;
-				for (int k = j; k < str.length; k++) {
+				// int x = 0;
+				// Integer[] ind = new Integer[10];
+				String stri = "";
+				for (int k = 0; k < str.length; k++) {
 					if (str[j].equals(str[k]) ) {
-						ind[x] = j;
-						x++;
+						stri += k + " ";
+						// x++;
 					}
 					
 				}
-				st.put(str[j], ind);
+				st.put(str[j], stri);
 			}
 			tree[i] = st;
 		}
@@ -30,14 +31,14 @@ class Solution {
 		for (int a = 0; a < 9 ; a++) {
 			int count = 0;
 			if (tree[a].contains(word)) {
-				Integer[] as = tree[a].get(word);
+				String[] as = tree[a].get(word).split(" ");
 				toString(a,as);
 				count++;	
 			}
 		}
 	}
 
-	public static void toString(int b, Integer[] arr) {
+	public static void toString(int b, String[] arr) {
 		String s = "";
 		s += b+", "+arr.length+", "+Arrays.toString(arr);
 		System.out.println(s);
